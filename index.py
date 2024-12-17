6032cc09177f22bd337f4a8212715fbbaa3bf0a9
Demo <matthcollin6@gmail.com>
2024-12-17 11:09:11 +0100

    refactor

--- index.py
+++ index.py
@@ -2,10 +2,10 @@ import re
 import http.server
 import socketserver
 import requests
+from datetime import date
 import csv
 
 f = open('index.html', 'w')
-
 
 
 html_template = """<html>
@@ -17,35 +17,51 @@ html_template = """<html>
     <ul>
 """
 
-def getCsv(): 
+def headRequest():
     url = "https://storage.googleapis.com/mollusques-caen/data.csv"
     # En-têtes pour spécifier la plage de bytes
-    headers = {"Range": "bytes=252240-252270"}
+    response = requests.head(url)
+    return int(response.headers.get('Content-Length'))
 
+def getData(): 
+    global html_template
+    url = "https://storage.googleapis.com/mollusques-caen/data.csv"
+    # En-têtes pour spécifier la plage de bytes
+    range_requests = "bytes=" + str(headRequest() - 32 * 24) + "-" + str(headRequest())
+    headers = {"Range": range_requests}
     # Faire la requête GET avec la plage de bytes
     response = requests.get(url, headers=headers)
     if response.status_code == 206 :
-        return csv.reader(response.text)
+        rows = response.text.splitlines()
+        generateCurrentMark(rows)
+        html_template += """
+        </ul>
+        </body> 
+        </html> """
         
-def printLine(line, html_template):
-    pattern = r"^(.*?),(.*)$"
-    match = re.match(pattern, line[2])
-    if match:
-        timestamp = match.group(1)  # First group: timestamp
-        value = float(match.group(2))
-
-    html_template += "<li> Date : "  + timestamp + " Note : " + str(round(value/2))  + "</li>"
     return html_template
 
+def generateCurrentMark( rows):
+    global html_template
+    html_template += "<p> La note du " + str(date.today) + "est de " + averageDay(rows)
+            
+def getRowData(row):
+    pattern = r"^(.*?),(.*)$"
+    match = re.match(pattern, row)
+    timestamp = match.group(1)  # First group: timestamp
+    value = float(match.group(2))
+    
+    return [timestamp, value]
 
-for row in getCsv():
-    printLine(row, html_template)
-    html_template += """
-    </ul>
-    </body> 
-    </html> 
-    """
+def averageDay(rows):
+    mark = 0
+    for row in rows: 
+        data = getRowData(row)
+        mark += round(data[1]/2)
+    return str(round(mark / 24))
+        
 
+getData()
 # writing the code into the file 
 f.write(html_template) 
 
@@ -54,9 +70,6 @@ f.close()
 
 PORT = 8080
 Handler = http.server.SimpleHTTPRequestHandler
-
-spamreader = []
-
 
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
     print("serving at port", PORT)
